2343e5cfdb6912790ac5cc4f7dde6a50cd888818
mannycolon <colonmanuel7@gmail.com>
2016-12-01 19:53:54 -0500

    moved the navbar to its own script and included this script in index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<html>
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>junkReSell</title>
-      <!-- Bootstrap -->
-      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <body>
-        <?php
-            echo "<p>Hello World!</p>";
-        ?>
-        <a href="login.php"> Login</a>
-        <a href="register.php"> Register</a>
-        <a href="homepage.php"> Homepage</a>
-    </body>
+<?php include 'view/header.php'; ?>
+  <main class="nofloat">
+    <h1>Products</h1>
+
+    </main>
+  </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -1,25 +1,32 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>junkReSell Home Page</title>
-        <!-- Bootstrap -->
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <body>
+<!-- the head section -->
+  <head>
+    <title>junkReSell</title>
+    <link rel="stylesheet" type="text/css"
+          href="bootstrap/css/bootstrap.min.css">
+    <style>
+    body{
+      padding-top: 40px;
+    }
+    </style>
+  </head>
+  <body>
+      <header>
         <!--will need to make this navigation bar standard throughout website-->
         <nav class="navbar navbar-inverse navbar-fixed-top">
           <div class="container">
             <ul class="nav navbar-nav">
-            <li class="active"><a href="homepage.php">Home</a> </li>
+              <li class="active"><a href="homepage.php">Home</a> </li>
               <li><a href="categories.php">Categories</a></li>
+              <li><a href="admin.php">Admin</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
               <li><a href="login.php">Login</a> </li>
               <li><a href="register.php">Register</a> </li>
               <li><a href="cart.php">My Cart</a> </li>
               <li><a href="checkout.php">Checkout</a> </li>
-              <li><a href="admin.php">Admin</a></li>
             </ul>
-
          </div>
         </nav>
-
-    </body>
-</html>
+      </header>
