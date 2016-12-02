eaba64103e2dcef00fac5a766856838327ef3371
mannycolon <colonmanuel7@gmail.com>
2016-12-02 12:13:04 -0500

    created fileupload script to upload an image to the images folder from the users account page

--- account/index.php
+++ account/index.php
@@ -18,5 +18,12 @@
     <h2>Acount</h2>
     <p> Hello <?php print "$user"?>!</p>
     <a href="logout.php">logout</a>
+
+    <!--uploading an image got the product-->
+    <form action="fileupload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
   </body>
 </html>
