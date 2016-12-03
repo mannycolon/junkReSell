c41d26f35f5d630f16e50cb4aeafcb898c427eda
mannycolon <colonmanuel7@gmail.com>
2016-12-02 23:05:31 -0500

    updated database so that it works on my end

--- create_db/junkReSell_db.sql
+++ create_db/junkReSell_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2016 at 06:45 PM
+-- Generation Time: Dec 03, 2016 at 05:03 AM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -19,6 +19,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `junkReSell_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryID` int(11) NOT NULL,
+  `categoryName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categoryID`, `categoryName`) VALUES
+(1, 'Electronics'),
+(2, 'Sporting Goods'),
+(3, 'Home and Garden'),
+(4, 'Clothing'),
+(5, 'Other');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `productID` int(11) NOT NULL,
+  `categoryID` int(11) NOT NULL,
+  `productName` varchar(100) NOT NULL,
+  `productPrice` decimal(10,2) NOT NULL,
+  `description` text NOT NULL,
+  `dateAdded` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`productID`, `categoryID`, `productName`, `productPrice`, `description`, `dateAdded`) VALUES
+(1, 1, 'Samsung 720p LED TV (2014 Model)', '650.00', 'Ideal for placement in a bedroom or dorm room, this \r\n 720p Samsung HDTV delivers a wide variety of colors and clarity, allowing you to relax and enjoy your \r\n favorite movies and shows. Connect your USB device directly for access to your photos, videos and music.', '2016-09-23');
 
 -- --------------------------------------------------------
 
@@ -48,6 +92,18 @@ INSERT INTO `users` (`userID`, `email`, `password`, `firstname`, `lastname`, `sh
 --
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -58,6 +114,11 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
@@ -65,46 +126,3 @@ ALTER TABLE `users`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
---
--- Table structure for table `category`
--- 
-CREATE TABLE `category` (
-  `categoryID` int(11) NOT NULL AUTO_INCREMENT,
-  `categoryName` varchar(255) NOT NULL,
-  PRIMARY KEY (categoryID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
- 
---
--- Dumping data for table `category`
---
-INSERT INTO `category`(`categoryID`, `categoryName`) VALUES 
-(1,'Electronics')
-(2,'Sporting Goods'),
-(3,'Home and Garden'),
-(4,'Clothing'),
-(5,'Other');
-
---
--- Table structure for table `product`
---
-
-CREATE TABLE `product` (
-  `productID` int(11) NOT NULL AUTO_INCREMENT,
-  `categoryID` int(11) NOT NULL,
-  `productName` varchar(100) NOT NULL,
-  `productPrice` decimal(10,2) NOT NULL,
-  `description` text NOT NULL,
-  `dateAdded` date NOT NULL,
-  PRIMARY KEY (productID)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`productID`, `categoryID`, `productName`, `productPrice`, `description`, `dateAdded`) VALUES
-(1, 1, 'Samsung 720p LED TV (2014 Model)', '650.00', 'Ideal for placement in a bedroom or dorm room, this 
- 720p Samsung HDTV delivers a wide variety of colors and clarity, allowing you to relax and enjoy your 
- favorite movies and shows. Connect your USB device directly for access to your photos, videos and music.', '2016-09-23');
- 
