3b85818c4bfe25e2812419abb32d8683a13e57b0
mannycolon <colonmanuel7@gmail.com>
2016-12-05 11:13:02 -0500

    added missing methods to cart class in cart.php file (update(), saveCart(), remove(), destroy() )

--- cart/cart.php
+++ cart/cart.php
@@ -1,5 +1,5 @@
 <?php session_start();
-class Cart {
+c{
     protected $cartContents = array();
     public function __construct(){
       // get the shopping cart array from the session
@@ -99,6 +99,83 @@ class Cart {
      * @param: array
      * @return: bool
      */
+     public function update($item = array()){
+        if(!is_array($item) OR count($item) === 0){
+          return FLASE;
+        }else{
+          if(!isset($item['rowid'], $this->cartContents[$item['rowid']])){
+            return FALSE;
+          }else{
+            // prep the quantity
+            if(isset($item['qty'])){
+              $item['qty'] = (float) $item['qty'];
+              // remove the item from the cart, if quantity is zero
+              if($item['qty'] == 0){
+                unset($this->cartContents[$item['rowid']]);
+                return TRUE;
+              }
+            }
+            // find updatable keys
+            $keys = array_intersect(array_keys($this->cartContents[$item['rowid']]), array_keys($item));
+            // prep the price
+            if(isset($item['price'])){
+              $item['price'] = (float) $item['price'];
+            }
+            // product id & name shouldn't be changed
+            foreach(array_dif($keys, array('id', 'name')) as $key){
+              $this->cartContents[$item['rowid']][$key] = $item[$key];
+            }
+            // save cart data
+            $this->saveCart();
+            return TRUE;
+          }
+        }
+     }
 
+     /**
+     * Save the cart array to the session
+     * @return: bool
+     */
+      protected function saveCart(){
+          $this->cartContents['totalItems'] = $this->cartContents['cartTotal'] = 0;
+          foreach($this->cartContents as $key => $val){
+            // make sure the array contains the proper indexes
+            if(!is_array($val) OR !isset($val['price'], $val['qty'])){
+              continue;
+            }
+            $this->cartContents['cartTotal'] += ($val['price'] * $val['qty']);
+            $this->cartContents['totalItems'] += ($val['qty'];
+            $this->cartContents[$key]['subtotal'] = ($this->cartContents[$key]['price'] * $this->cartContents[$key]['qty']);
+          }
 
+          // if cart empty, delete it from the session
+          if(count($this->cartContents) <= 2){
+            unset($_SESSION['cartContents']);
+            return FALSE;
+          }else{
+            $_SESSION['cartContents'] = $this->cartContents;
+            return TRUE;
+          }
+      }
+
+      /**
+     * Remove Item: Removes an item from the cart
+     * @param: int
+     * @return: bool
+     */
+     public function remove($row_id){
+        // unset & save
+        unset($this->cartContents[$row_id]);
+        $this->saveCart();
+        return TRUE;
+     }
+
+     /**
+     * Destroy the cart: Empties the cart and destroy the session
+     * @return: void
+     */
+    public function destroy(){
+        $this->cartContents = array('cartTotal' => 0, 'totalItems' => 0);
+        unset($_SESSION['cartContents']);
+    }
 }
