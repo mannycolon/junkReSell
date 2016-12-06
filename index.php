c3cb84d616365763095086c8e4229bd11c687469
mannycolon <colonmanuel7@gmail.com>
2016-12-05 20:05:37 -0500

    cleaned up index.php (homepage) remove tabs

--- index.php
+++ index.php
@@ -3,43 +3,42 @@
 <?php include 'view/header.php'; ?>
   <main class="nofloat">
     <br>
-	<table>
-	    <?php include 'category/categoryfunctions.php';
+    <table>
+      <?php include 'category/categoryfunctions.php';
 
-			$category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
-			if ($category_id == NULL || $category_id == FALSE)
-			{
-			    $category_id = rand(1, 5);
-			}
-			// Get name for selected category
-			$queryCategory = 'SELECT * FROM category
-			                      WHERE categoryID = :category_id';
-			$statement1 = $db->prepare($queryCategory);
-			$statement1->bindValue(':category_id', $category_id);
-			$statement1->execute();
-			$category = $statement1->fetch();
-			$category_name = $category['categoryName'];
-			$statement1->closeCursor();
+      $category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
+      if ($category_id == NULL || $category_id == FALSE){
+        $category_id = rand(1, 5);
+      }
+      // Get name for selected category
+      $queryCategory = 'SELECT * FROM category
+                        WHERE categoryID = :category_id';
+      $statement1 = $db->prepare($queryCategory);
+      $statement1->bindValue(':category_id', $category_id);
+      $statement1->execute();
+      $category = $statement1->fetch();
+      $category_name = $category['categoryName'];
+      $statement1->closeCursor();
 
-			// Get all categories
-			$queryAllCategories = 'SELECT * FROM category
-			                           ORDER BY categoryID';
-			$statement2 = $db->prepare($queryAllCategories);
-			$statement2->execute();
-			$categories = $statement2->fetchAll();
-			$statement2->closeCursor();
+      // Get all categories
+      $queryAllCategories = 'SELECT * FROM category
+                             ORDER BY categoryID';
+      $statement2 = $db->prepare($queryAllCategories);
+      $statement2->execute();
+      $categories = $statement2->fetchAll();
+      $statement2->closeCursor();
 
-			// Get products for selected category
-			$queryProducts = 'SELECT * FROM product
-			              WHERE categoryID = :category_id
-			              ORDER BY productID';
-			$statement3 = $db->prepare($queryProducts);
-			$statement3->bindValue(':category_id', $category_id);
-			$statement3->execute();
-			$products = $statement3->fetchAll();
-			$statement3->closeCursor();
+      // Get products for selected category
+      $queryProducts = 'SELECT * FROM product
+                        WHERE categoryID = :category_id
+                        ORDER BY productID';
+      $statement3 = $db->prepare($queryProducts);
+      $statement3->bindValue(':category_id', $category_id);
+      $statement3->execute();
+      $products = $statement3->fetchAll();
+      $statement3->closeCursor();
 
-			?>
+      ?>
 <!DOCTYPE html>
 <html>
 <!-- the head section -->
