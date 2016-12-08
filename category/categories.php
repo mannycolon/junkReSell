d2912613225ee7cdac10ae8241d9dfd2cd8f05bb
gvrojas <grojas1212@gmail.com>
2016-12-08 12:48:07 -0500

    added css, removed unnecessary html

--- category/categories.php
+++ category/categories.php
@@ -4,27 +4,14 @@
   <main class="nofloat">
     <br>
         <?php include 'categoryfunctions.php'; ?>
-
-<!DOCTYPE html>
-<html>
-<!-- the head section -->
-<head>
-    <title>junkReSell Homepage</title>
-</head>
-
-<!-- the body section -->
-<body>
-<main>
-    <h1>junkReSell Homepage</h1>
     <aside>
         <!-- display a list of categories -->
-        <h2>Categories</h2>
-        <nav>
+        <h2 style="padding-left: 60px; padding-right: 60px">Categories</h2>
+        <div class="list-group" style="padding-left: 20px; padding-right: 60px">
             <?php echo all_categories() ?>
-        </nav>
+        </div>
     </aside>
 
 </main>
-<footer></footer>
 </body>
 </html>
