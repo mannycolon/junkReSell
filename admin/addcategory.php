5a3d75a69747153704d9b4418f0feecf8223ec1b
gvrojas <grojas1212@gmail.com>
2016-12-08 10:58:03 -0500

    allows admin to create a category

---
+++ admin/addcategory.php
@@ -0,0 +1,18 @@
+<?php require_once('../util/main.php'); ?>
+<?php include '../view/header.php'; ?>
+	<main>
+			<h1 align="center">Add Category </h1>
+			<form method="POST" action="addcategoryconfirm.php" style="padding-left: 600px; padding-right: 600px">
+
+			<div class="form-group">
+				<h3>Category:</h3>
+	            <input type="text" name="category_name" class="form-control"><br><br>
+            </div>
+			<input id="button" type="submit" name="submit" value="Submit" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="background-color: #29a329; font-size: 20px; border-color:#29a329;">
+			</form>
+		
+	</main>
+
+	</body>
+
+</html>
