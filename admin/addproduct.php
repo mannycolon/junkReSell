136ac590595920eb89c67fbbc506014d0bf5a20a
gvrojas <grojas1212@gmail.com>
2016-12-08 18:23:42 -0500

    added image file upload when admin adds product

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -76,6 +76,12 @@
 		       data-toggle="dropdown" style="background-color: #29a329; font-size: 20px; border-color:#29a329;">
 	</form>
 
+	<form action="../account/fileupload.php" method="post" enctype="multipart/form-data" style="padding-left: 600px; padding-right: 600px">
+        	<h4>Select image to upload before clicking "Submit":</h4>
+        	<input type="file" name="fileToUpload" id="fileToUpload"><br>
+        <input type="submit" value="Upload Image" name="submit">
+    	</form>
+	    
 	</main>
 	<br><p align="center"><a href="index.php">Return to Admin Main Page</a></p>
     </body>
