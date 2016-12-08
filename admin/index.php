39268e6a1afa9c7b9daed8bbb74c8d1deea6d2e1
gvrojas <grojas1212@gmail.com>
2016-12-08 18:55:27 -0500

    added view products and categories links

--- admin/index.php
+++ admin/index.php
@@ -11,17 +11,15 @@
 <?php include '../view/header.php'; ?>
     <main class="nofloat">
       <h2 align="center">Admin Account</h2>
-      <h3 align="center"> Hello, <?php print "$user"?>!</h3>
-      <form action="fileupload.php" method="post" enctype="multipart/form-data" style="padding-left: 740px; padding-right: 740px">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload"><br>
-        <input type="submit" value="Upload Image" name="submit">
-      </form>
-    <h2 align="center">Choose an option below:</h2>
-    <p align="center"><a href="addproduct.php" class="btn btn-info" role="button">Add Product</a>&nbsp
-    <a href="addcategory.php" class="btn btn-info" role="button">Add Category</a></p>
-    <p align="center"><a href="viewadmins.php" class="btn btn-info" role="button">View Admins</a>&nbsp&nbsp
-    <a href="viewusers.php" class="btn btn-info" role="button">View Users</a></p>
+      <h3 align="center" style="color:navy;"> Hello, <?php print "$user"?>!</h3><br>
+
+      <h4 align="center">Choose an option below:</h4>
+      <p align="center"><a href="addproduct.php" class="btn btn-info" role="button">Add Product</a>&nbsp
+      <a href="addcategory.php" class="btn btn-info" role="button">Add Category</a></p>
+      <p align="center"><a href="viewadmins.php" class="btn btn-info" role="button">View Admins</a>&nbsp&nbsp
+      <a href="viewusers.php" class="btn btn-info" role="button">View Users</a>&nbsp&nbsp
+      <a href="viewproducts.php" class="btn btn-info" role="button">View Products</a>&nbsp&nbsp
+      <a href="viewcategories.php" class="btn btn-info" role="button">View Categories</a></p>
     </main>
   </body>
 </html>
