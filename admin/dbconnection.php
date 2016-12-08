dfcd34f7bdf83c5af9b1f7279d1b429812fecf9f
gvrojas <grojas1212@gmail.com>
2016-12-08 10:54:46 -0500

    adding connection to database file

---
+++ admin/dbconnection.php
@@ -0,0 +1,16 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=junkReSell_db';
+    $username = 'root';
+    $password = '';
+
+    try
+    {
+        $db = new PDO($dsn, $username, $password);
+    }
+    catch (PDOException $e)
+    {
+        $error_message = $e->getMessage();
+        echo '<p>Not connected to database</p>';
+        exit();
+    }
+?>
